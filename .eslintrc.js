a0fcaaa9216ff6fd5df3551ed4e15bf159a8c1a1
Habeebability <awoyemi.habeeb@gmail.com>
2025-08-26 07:42:07 +0100

    Final deployment fixes: Disable problematic ESLint config, ensure all builds pass for Render deployment

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'warn',
+    'prefer-const': 'error',
+  },
+  ignorePatterns: [
+    'dist/',
+    'node_modules/',
+  ],
+};
